ea5c9559de41e10102753bc4295d59635f0539ec
Pedram Mohammadalipour <p.mohammadalipour@Dotin.ir>
2024-05-08 08:49:03 +0330

    initial commit

---
+++ lib/internal_rounting_test_app.dart
@@ -0,0 +1,14 @@
+library internal_rounting_test_app;
+
+import 'package:flutter/material.dart';
+
+class InitialRoutePage extends StatelessWidget {
+  const InitialRoutePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
